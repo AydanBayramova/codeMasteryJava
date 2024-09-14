2857e733fa87bdb5c4a938eabe1a556347a12b7b
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-14 16:27:28 +0400

    Given an 0-indexed array of integers arr[] of size n, find its peak element and return it's index. An element is considered to be peak if it's value is greater than or equal to the values of its adjacent elements (if they exist).

---
+++ src/main/java/az/edu/turing/greeksForGreeks/PeakNumber.java
@@ -0,0 +1,14 @@
+package az.edu.turing.greeksForGreeks;
+
+public class PeakNumber {
+
+    public int peakNum(int[] arr, int n) {
+        if (n == 1) return 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 && arr[i] > arr[i + 1]) return i;
+            else if (i == n - 1 && arr[i] > arr[i - 1]) return i;
+            else if (arr[i] >= arr[i + 1] && arr[i] >= arr[i - 1]) return i;
+        }
+        return -1;
+    }
+}
