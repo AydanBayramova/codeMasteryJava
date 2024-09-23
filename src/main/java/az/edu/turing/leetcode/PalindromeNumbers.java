d47ff660d02f0252e380692eaecaadcad208019e
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-09-23 14:13:08 +0400

    Find Palindrome number

--- src/main/java/az/edu/turing/leetcode/PalindromeNumbers.java
+++ src/main/java/az/edu/turing/leetcode/PalindromeNumbers.java
@@ -2,11 +2,25 @@ package az.edu.turing.leetcode;
 
 public class PalindromeNumbers {
     public static boolean isPalindrome(int x) {
-        String str = String.valueOf(x);
-        String strb=new StringBuilder(str).reverse().toString();
-        if (str.equals(strb)){
-            return true;
+//        String str = String.valueOf(x);
+//        String strb=new StringBuilder(str).reverse().toString();
+//        if (str.equals(strb)){
+//            return true;
+//        }
+//        return false;
+
+        if (x < 0) return false;
+        int rev = 0;
+        int number = x;
+        while (x!= 0){
+            rev = rev*10 + x%10;
+            x = x/10;
+
         }
+        if (rev == number) return true;
         return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(101));
     }
 }
