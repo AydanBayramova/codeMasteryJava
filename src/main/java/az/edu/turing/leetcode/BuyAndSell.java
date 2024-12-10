20d93de8983568e5e6026f013b4ddd6411c8731d
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-12-11 01:12:35 +0400

    You are given an array prices where prices[i] is the price of a given stock on the ith day.
    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
    
    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.

---
+++ src/main/java/az/edu/turing/leetcode/BuyAndSell.java
@@ -0,0 +1,16 @@
+package az.edu.turing.leetcode;
+
+public class BuyAndSell {
+    public int maxProfit(int[] prices) {
+       int maxProfit = 0;
+       int buy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > maxProfit) {
+                maxProfit = prices[i] - buy;
+            }
+        }
+        return maxProfit;
+    }
+}
