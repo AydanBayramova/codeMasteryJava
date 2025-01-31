dfe91f6e28edb37cfbaa17ed14086f23652d7587
AydanBayramova <bayramoffaaydan1@gmail.com>
2025-01-31 15:33:48 +0400

    Find MaxNumberofArray use for loop O(n) complexity

---
+++ src/main/java/az/edu/turing/dataStructures/MaxNumberofArray.java
@@ -0,0 +1,15 @@
+package az.edu.turing.dataStructures;
+
+public class MaxNumberofArray {
+    public static void main(String[] args) {}
+    public int maxNumberofArray(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+
+            }
+        }
+        return max;
+    }
+}
