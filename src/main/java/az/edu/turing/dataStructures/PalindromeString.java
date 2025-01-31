d0357fe892c28558b742cef280e7a35d082164b6
AydanBayramova <bayramoffaaydan1@gmail.com>
2025-01-31 16:01:31 +0400

    Find Palindrome of String

---
+++ src/main/java/az/edu/turing/dataStructures/PalindromeString.java
@@ -0,0 +1,19 @@
+package az.edu.turing.dataStructures;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s1 = "java";
+        System.out.println(isPalindrome(s1));
+    }
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+                left++;
+                right--;
+        }
+        return true;
+    }
+}
